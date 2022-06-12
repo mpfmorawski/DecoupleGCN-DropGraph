54648ed897544ca9539cba716bf0afe834d45e2d
mmorawsk <m.morawski@stud.elka.pw.edu.pl>
2022-06-12 22:46:02 +0200

    Repairing relative path in ntu_gendata.py

--- data_gen/ntu_gendata.py
+++ data_gen/ntu_gendata.py
@@ -148,10 +148,10 @@ def gendata(data_path, out_path, ignored_sample_path=None, benchmark='xview', pa
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='NTU-RGB-D Data Converter.')
-    parser.add_argument('--data_path', default='../data/nturgbd_raw')
+    parser.add_argument('--data_path', default='data/nturgbd_raw')
     parser.add_argument('--ignored_sample_path',
-                        default='../data/nturgbd_raw/NTU_RGBD_samples_with_missing_skeletons.txt')
-    parser.add_argument('--out_folder', default='../data/ntu/')
+                        default='data/nturgbd_raw/NTU_RGBD_samples_with_missing_skeletons.txt')
+    parser.add_argument('--out_folder', default='data/ntu/')
 
     benchmark = ['xsub', 'xview']
     part = ['train', 'val']
