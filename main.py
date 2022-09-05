d1952d24182d4ad0594d757850509e2028dd0044
mpfmorawski <m.morawski@stud.elka.pw.edu.pl>
2022-09-05 19:16:07 +0200

    Usage of yaml.safe_load instead of yaml.load

--- main.py
+++ main.py
@@ -657,7 +657,7 @@ if __name__ == "__main__":
 
     if p.config is not None:
         with open(p.config, "r") as f:
-            default_arg = yaml.load(f)
+            default_arg = yaml.safe_load(f)
         key = vars(p).keys()
         for k in default_arg.keys():
             if k not in key:
